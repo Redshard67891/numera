65579074616eccfc8c2a9f8144b722b4ac9b3f9b
Devoe John <devoejohn7@gmail.com>
2025-09-03 00:24:59 +0000

    good, now Let's add this to Google search console so we can begin to mon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
+        {/* Add Google Search Console verification tag here */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
